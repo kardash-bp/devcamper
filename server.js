28c06b970c3c84635de663f67a492496761c2881
kardash-bp <kardasbp@gmail.com>
2020-05-29 23:40:03 +0200

    .json for postman import

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const expressRate = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const fileupload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const sanitize = require('express-mongo-sanitize')
 // imports
 const errorHandler = require('./middleware/error')
 require('./db')
@@ -14,12 +20,27 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())
+// sanitize data prevent SQL injection
+app.use(sanitize())
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 app.use(fileupload())
-
+// set security headers
+app.use(helmet())
+// prevent XSS attacks
+app.use(xss())
+// Rate limit
+const limiter = expressRate({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+})
+app.use(limiter)
+// Prevent http param pollution
+app.use(hpp())
+// Enable CORS
+app.use(cors())
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
