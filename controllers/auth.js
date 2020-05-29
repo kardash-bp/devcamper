a8ef355a62fe024e86a981eb71fc0e312c92616f
kardash-bp <kardasbp@gmail.com>
2020-05-29 17:13:22 +0200

    logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,6 +43,20 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 })
 
+// @desc   Log user out / clear cookie
+// @route  GET /api/v1/auth/logout
+// @access private
+exports.logout = asyncHandler(async (req, res, next) => {
+  req.headers.authorization = ''
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 5 * 1000),
+    httpOnly: true
+  })
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
+})
 // @desc   Get current logged user
 // @route  POST /api/v1/auth/me
 // @access private
--- middleware/auth.js
+++ middleware/auth.js
@@ -7,8 +7,12 @@ const User = require('../models/User')
 exports.protect = asyncHandler(async (req, res, next) => {
   let token
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // set token from header
     token = req.headers.authorization.split(' ')[1]
-  } // else if (req.cookies.token) token = req.cookies.token
+  } else if (req.cookies.token) {
+    // set token from cookie
+    token = req.cookies.token
+  }
   // Make sure token exists
   if (!token) return next(new ErrorResponse('Not authorized to access this route', 401))
   try {
